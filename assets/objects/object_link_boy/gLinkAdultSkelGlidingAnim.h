42cb3d159e67e7b3b6d67508d285555247fb6766
AbdooOwd <benari102@gmail.com>
2025-07-23 11:12:50 +0200

    [data-only] Link Gliding Anim

---
+++ assets/objects/object_link_boy/gLinkAdultSkelGlidingAnim.h
@@ -0,0 +1,10 @@
+#ifndef ADULT_LINK_ANIM_GLIDING_H
+#define ADULT_LINK_ANIM_GLIDING_H
+
+#include "ultra64.h"
+#include "array_count.h"
+#include "z64animation.h"
+
+extern LinkAnimationHeader gLinkAdultSkelGlidingAnim;
+
+#endif
